2ac0695db8597e5d7440b9c356ca107f7228716b
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-16 15:53:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-west-1" # Change to your desired AWS region
 }
 
+   backend "s3" {
+       bucket = "[tester]"
+       region = "west-us-1"
+   }
+}
+
 # DynamoDB Table
 resource "aws_dynamodb_table" "example" {
   name           = "example_table"
