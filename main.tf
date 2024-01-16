4a28feea4577517eafea6ec7e0b678359a8ddb56
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-16 18:32:33 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "s3buckettesting12234234"
+  acl    = "private"  # Access control list, adjust as needed
+
+  tags = {
+    Name        = "ExampleS3Bucket"
+    Environment = "Production"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.example.bucket
+    target_prefix = "logs/"
+  }
+
+  lifecycle {
+    prevent_destroy = false  # Set to true to prevent accidental deletion
+  }
+
+  # Add other configurations as needed
+}
