2a8d818f68e38488c16dc27cb4c3ddecc1c593eb
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-16 19:03:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" {
+resource "aws_s3_bucket" " "s3bucket" {
   bucket = "s3buckettesting12234234"
   acl    = "private"  # Access control list, adjust as needed
 
