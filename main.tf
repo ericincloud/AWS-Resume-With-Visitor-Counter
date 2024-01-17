39783db683d4a91b41cb5edda2150807697c48cf
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-16 19:10:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,6 @@ resource "aws_s3_bucket" "test23423" {
     enabled = true
   }
 
-  logging {
-    target_bucket = aws_s3_bucket.example.bucket
-    target_prefix = "logs/"
-  }
-
   lifecycle {
     prevent_destroy = false  # Set to true to prevent accidental deletion
   }
