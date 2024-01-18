941f35627925448f8b1faa27df6d453469c2c761
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-18 15:32:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-1"  # Change to your desired AWS region
 }
 
-resource "aws_s3_bucket" {
+resource "aws_s3_bucket" "StaticSiteEricInCloud" {
   bucket = "ericincloud.com"
   acl    = "public-read"
  
