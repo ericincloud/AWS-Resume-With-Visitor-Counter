e95d2e46e670c8834aef4670bf5656ae54bef95b
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-18 15:34:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "StaticSiteEricInCloud" {
   bucket = "ericincloud.com"
-  acl    = "public-read"
+  acl    = "private"
  
   website {
     index_document = "index.html"
