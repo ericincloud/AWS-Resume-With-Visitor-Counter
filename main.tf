6614818f936187771db080835a6bc0594127f73a
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-19 14:04:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_s3_bucket_public_access_block" "StaticSite" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
  
   versioning {
     enabled = false
