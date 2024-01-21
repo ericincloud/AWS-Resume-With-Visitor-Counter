ae4c27bcc214e5724b3a291b41539b138d7d4192
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-01-21 16:15:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,6 +136,7 @@ resource "aws_dynamodb_table" "visitor_table" {
 resource "aws_lambda_function" "visitor_counter" {
   function_name = "VisitorCounter"
   runtime       = "python3.12"
+  handler       = "lambda_function.lambda_handler"
   role          = aws_iam_role.lambda_exec.arn
   filename      = "VisitorCountLambda.zip"  # Specify the path to your Lambda code
 
@@ -145,9 +146,9 @@ resource "aws_lambda_function" "visitor_counter" {
 resource "aws_lambda_function" "retrieve_visitor_count" {
   function_name = "RetrieveVisitorCount"
   runtime       = "python3.12"
+  handler       = "lambda_function.lambda_handler"
   role          = aws_iam_role.lambda_exec.arn
   filename      = "RetrieveVisitorCountLambda.zip"  # Specify the path to your Lambda code
-  handler       = "lambda_function.lambda_handler"
   source_code_hash = filebase64("RetrieveVisitorCountLambda.zip")
 
 }
@@ -155,7 +156,6 @@ resource "aws_lambda_function" "retrieve_visitor_count" {
 # IAM Role for Lambda Execution
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
-  handler       = "lambda_function.lambda_handler"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
